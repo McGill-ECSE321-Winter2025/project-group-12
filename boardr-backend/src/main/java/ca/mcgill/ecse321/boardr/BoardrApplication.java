6914beeb7ca61bdac697df6246cac2334c50c35f
Yoonjung Choi <gracechoi1357@gmail.com>
2025-02-05 22:00:32 -0500

    Made .env automatically get loaded for DB username and password

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/BoardrApplication.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/BoardrApplication.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.boardr;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardrApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Set system properties for Spring Boot to use
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(BoardrApplication.class, args);
 	}
 
