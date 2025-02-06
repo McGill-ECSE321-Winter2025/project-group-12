1d7d9ed2f984724f03c7577d51dc388304a03945
Yoonjung Choi <gracechoi1357@gmail.com>
2025-02-05 21:51:21 -0500

    Working Spring Boot application with PostgreSQL

---
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/BoardrApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.boardr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardrApplication.class, args);
+	}
+
+}
---
+++ boardr-backend/src/test/java/ca/mcgill/ecse321/boardr/BoardrApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.boardr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
