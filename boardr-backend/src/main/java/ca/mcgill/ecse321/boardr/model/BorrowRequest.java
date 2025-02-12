3b8b7e37ede3c918cac7ce11b877664f0ed0f1fa
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-02-11 19:39:48 -0500

    [model-code]- Create BorrowRequest

---
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/BorrowRequest.java
@@ -0,0 +1,65 @@
+package ca.mcgill.ecse321.boardr.model;
+
+import jakarta.persistence.*;
+
+import javax.swing.text.SimpleAttributeSet;
+import java.sql.Date;
+
+@Entity
+public class BorrowRequest {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "board_game_instance_id")
+    private BoardGameInstance boardGameInstance;
+
+    @OneToOne
+    @JoinColumn(name = "borrower_id")
+    private UserAccount userAccount;
+
+    private Date requestDate;
+    private Date returnDate;
+    private Status status;
+
+    public enum Status {
+        Pending,
+        Accepted,
+        Declined;
+    }
+
+    BorrowRequest(){};
+
+    BorrowRequest(BoardGameInstance boardGameInstance, UserAccount userAccount, Date requestDate, Date returnDate) {
+        this.boardGameInstance = boardGameInstance;
+        this.userAccount = userAccount;
+        this.requestDate = requestDate;
+        this.returnDate = returnDate;
+        this.status = Status.Pending;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public BoardGameInstance getBoardGameInstance() {
+        return boardGameInstance;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+}
