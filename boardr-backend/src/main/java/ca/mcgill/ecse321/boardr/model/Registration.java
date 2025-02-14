800c0e6f9804b37d199986afefeb00263c4d3471
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-02-14 15:04:55 -0500

    FIX: Change Registration primaryId to compositeKey

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/Registration.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/Registration.java
@@ -1,52 +1,74 @@
 package ca.mcgill.ecse321.boardr.model;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.sql.Date;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import java.util.Objects;
 
 
 @Entity
 public class Registration {
-    @Id
-    @GeneratedValue
-    private int registrationId;
+    @EmbeddedId
+    private RegistrationKey registrationKey;
     private Date registrationDate;
-
-    @ManyToOne
-    @JoinColumn(name = "event_id")
-    private Event event;
-
-    @ManyToOne
-    @JoinColumn(name = "user_account_id")
-    private UserAccount userAccount;
 
     
     protected Registration() {}
 
-    public Registration(Event event, UserAccount userAccount) {
-        this.event = event;
-        this.userAccount = userAccount;
+    public Registration(RegistrationKey registrationKey) {
+        this.registrationKey = registrationKey;
         this.registrationDate = new Date(System.currentTimeMillis());
     }
 
-    public int getRegistrationId(){
-        return this.registrationId;
+    public RegistrationKey getRegistrationKey(){
+        return this.registrationKey;
     }
 
     public Date getRegistrationDate(){
         return this.registrationDate;
     }
 
-    public Event getEvent(){
-        return this.event;
-    }
     
-    public UserAccount getUserAccount(){
-        return this.userAccount;
+    @Embeddable
+    public static class RegistrationKey implements Serializable {
+        @ManyToOne
+        @JoinColumn(name = "user_account_id")
+        private UserAccount registrant;
+
+        @ManyToOne
+        @JoinColumn(name = "event_id")
+        private Event event;
+
+        public RegistrationKey() {
+        }
+
+        public RegistrationKey(UserAccount registrant, Event event) {
+            this.registrant = registrant;
+            this.event = event;
+        }
+
+        public UserAccount getRegistrant() {
+            return registrant;
+        }
+
+        public Event getEvent() {
+            return event;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof RegistrationKey)) {
+                return false;
+            }
+            RegistrationKey that = (RegistrationKey) obj;
+            return this.registrant.getUserAccountId() == that.registrant.getUserAccountId()
+                    && this.event.getEventId() == that.event.getEventId();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.registrant.getUserAccountId(), this.event.getEventId());
+        }
     }
 }
