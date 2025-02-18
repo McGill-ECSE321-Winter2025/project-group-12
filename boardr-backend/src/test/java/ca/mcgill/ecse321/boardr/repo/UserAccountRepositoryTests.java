4759d61e91c97e742b606493c788f7c62b4ee51b
Yoonjung Choi <gracechoi1357@gmail.com>
2025-02-18 16:38:10 -0500

    Implemented UserAccount Test

---
+++ boardr-backend/src/test/java/ca/mcgill/ecse321/boardr/repo/UserAccountRepositoryTests.java
@@ -0,0 +1,46 @@
+package ca.mcgill.ecse321.boardr.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.boardr.model.UserAccount;
+
+@SpringBootTest
+public class UserAccountRepositoryTests {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        userAccountRepository.deleteAll();
+    }
+
+    @Test
+    public void testCreateandReadUserAccount() {
+        
+        // Arrange
+        UserAccount userAccount = new UserAccount("testUser", "testuser@mail.mcgill.ca", "password");
+        userAccount = userAccountRepository.save(userAccount);
+
+        // Act
+        UserAccount retrievedUserAccount = userAccountRepository.findUserAccountById(userAccount.getUserAccountId());
+
+        // Assert
+        assertNotNull(retrievedUserAccount);
+        assertEquals(userAccount.getUserAccountId(), retrievedUserAccount.getUserAccountId());
+        assertEquals(userAccount.getCreationDate(), retrievedUserAccount.getCreationDate());
+        assertEquals(userAccount.getEmail(), retrievedUserAccount.getEmail());
+        assertEquals(userAccount.getName(), retrievedUserAccount.getName());
+        assertEquals(userAccount.getPassword(), retrievedUserAccount.getPassword());
+    
+    }
+}
