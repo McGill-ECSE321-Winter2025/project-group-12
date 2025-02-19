d3fe1f9941417cf6ac665f0a240e1e004ffa1a31
Yoonjung Choi <gracechoi1357@gmail.com>
2025-02-19 14:29:01 -0500

    Create BorrowRequest, Review and UserAccount Repositories

---
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/repo/ReviewRepository.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.boardr.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ca.mcgill.ecse321.boardr.model.Review;
+
+@Repository
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
+    public Review findByReviewId(int id);
+}
