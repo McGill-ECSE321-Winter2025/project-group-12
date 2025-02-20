c8a9367d10757784dc64ea7556c5f99e9ade9c6a
Ji-One1 <86013565+Ji-One1@users.noreply.github.com>
2025-02-19 21:14:57 -0500

    Add discriminator values (#32)

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/GameOwner.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/GameOwner.java
@@ -1,8 +1,10 @@
 package ca.mcgill.ecse321.boardr.model;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("GAMEOWNER")
 public class GameOwner extends UserRole {
     public GameOwner() {
     }
