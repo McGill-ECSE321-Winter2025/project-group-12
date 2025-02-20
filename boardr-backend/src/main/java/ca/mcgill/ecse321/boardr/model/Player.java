22709133e932267157b55f31ffa3655d8b4b184a
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-02-19 21:05:29 -0500

    Add discriminator values

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/Player.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/model/Player.java
@@ -1,6 +1,12 @@
 package ca.mcgill.ecse321.boardr.model;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("PLAYER")
 public class Player extends UserRole {
+
 
     public Player() {
     }
