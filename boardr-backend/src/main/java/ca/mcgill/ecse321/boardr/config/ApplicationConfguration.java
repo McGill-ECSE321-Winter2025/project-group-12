c063ad3a47a2269a0da5d69db48d227c11ed48ea
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-03-07 01:42:39 -0500

    Set up Spring Security

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/config/ApplicationConfguration.java
+++
@@ -1,4 +0,0 @@
-package ca.mcgill.ecse321.boardr.config;
-
-public class ApplicationConfguration {
-}
---
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/service/AuthenticationService.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.boardr.service;
+
+import ca.mcgill.ecse321.boardr.authentication.JwtService;
+import ca.mcgill.ecse321.boardr.dto.Authorization.AuthRequestDTO;
+import ca.mcgill.ecse321.boardr.dto.Authorization.AuthResponseDTO;
+import ca.mcgill.ecse321.boardr.model.UserAccount;
+import ca.mcgill.ecse321.boardr.repo.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+    @Autowired
+    JwtService jwtService;
+
+
+
+
+    public AuthResponseDTO login(AuthRequestDTO credentials) {
+        UserAccount user = userAccountRepository.findByEmail(credentials.getEmail()).orElseThrow(() -> new RuntimeException("Account not found"));
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+        return new AuthResponseDTO(jwtService.generateToken(user.getEmail()), user.getEmail(), user.getName());
+    }
+
+}
