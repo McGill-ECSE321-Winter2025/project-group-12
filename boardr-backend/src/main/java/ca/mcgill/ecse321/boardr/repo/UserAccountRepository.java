4466834c3f3616a49e690722b25913da76101667
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-03-06 20:28:29 -0500

    Create CustomUserDetailsService

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/repo/UserAccountRepository.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/repo/UserAccountRepository.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.boardr.repo;
 
+import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ca.mcgill.ecse321.boardr.model.UserAccount;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAccountRepository extends CrudRepository<UserAccount, Integer> {
-    public UserAccount findByUserAccountId(int id);
+    public Optional<UserAccount> findByUserAccountId(int id);
+    public Optional<UserAccount> findByEmail(String email);
 
 }
