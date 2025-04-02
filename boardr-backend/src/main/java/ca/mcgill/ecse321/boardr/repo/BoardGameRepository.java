17e1089f05829f0c1e42b50db2ba455dd6c4f048
Yoonjung Choi <gracechoi1357@gmail.com>
2025-04-02 17:25:17 -0400

    FEAT: Add findByName method to BoardGameRepository and validate board game name uniqueness in creation

--- boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/repo/BoardGameRepository.java
+++ boardr-backend/src/main/java/ca/mcgill/ecse321/boardr/repo/BoardGameRepository.java
@@ -3,8 +3,9 @@ package ca.mcgill.ecse321.boardr.repo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ca.mcgill.ecse321.boardr.model.BoardGame;
+import java.util.Optional;
 
 @Repository
 public interface BoardGameRepository extends CrudRepository<BoardGame, Integer> {
-    
+    Optional<BoardGame> findByName(String name);
 }
